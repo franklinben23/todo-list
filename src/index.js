5ea0e85377e9cf7adb2e90bd498fd359b6a05d76
franklin <franklinbenjamin032@gmail.com>
2021-12-03 15:51:42 -0400

    basic list structure

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const taskList = document.querySelector('.task-list');
 
-// Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-  return element;
-}
+let list = [{name: 'wash the dishes', id:1}, {name: 'walk the dog', id:2}, {name: 'dry the clothes', id:3}];
 
-document.body.appendChild(component());
+const render = () => {
+  list.forEach(task => {
+
+    const li = document.createElement('li');
+    li.classList.add('task')
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.classList.add('checkbox')
+
+    const label = document.createElement('label');
+    label.classList.add('task-label')
+
+    label.innerHTML = task.name
+
+    li.appendChild(checkbox)
+    li.appendChild(label)
+    taskList.appendChild(li)
+
+  })
+};
+
+render();
+
