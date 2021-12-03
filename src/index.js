7df1716e5c3b4ea84f7bc7a5b86fcbe732686d41
franklin <franklinbenjamin032@gmail.com>
2021-12-03 15:59:04 -0400

    linter fix

--- src/index.js
+++ src/index.js
@@ -1,32 +1,27 @@
-import _ from 'lodash';
 import './style.css';
 
 const taskList = document.querySelector('.task-list');
 
-
-let list = [{name: 'wash the dishes', id:1}, {name: 'walk the dog', id:2}, {name: 'dry the clothes', id:3}];
+const list = [{ name: 'wash the dishes', id: 1 }, { name: 'walk the dog', id: 2 }, { name: 'dry the clothes', id: 3 }];
 
 const render = () => {
-  list.forEach(task => {
-
+  list.forEach((task) => {
     const li = document.createElement('li');
-    li.classList.add('task')
+    li.classList.add('task');
 
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.classList.add('checkbox')
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.classList.add('checkbox');
 
     const label = document.createElement('label');
-    label.classList.add('task-label')
+    label.classList.add('task-label');
 
-    label.innerHTML = task.name
+    label.innerHTML = task.name;
 
-    li.appendChild(checkbox)
-    li.appendChild(label)
-    taskList.appendChild(li)
-
-  })
+    li.appendChild(checkbox);
+    li.appendChild(label);
+    taskList.appendChild(li);
+  });
 };
 
 render();
-
