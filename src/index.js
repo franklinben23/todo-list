610f09f2db2f33d0df27af9d00292b562a98f255
franklin <franklinbenjamin032@gmail.com>
2021-12-04 00:02:06 -0400

    make list interactive

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import './style.css';
+import { updateToTrue, updateToFalse } from './updateStatus.js';
 
 const taskList = document.querySelector('.task-list');
 
 const list = [{ name: 'wash the dishes', id: 1, completed: false }, { name: 'walk the dog', id: 2, completed: false }, { name: 'dry the clothes', id: 3, completed: false }];
+
+const record = () => {
+  localStorage.setItem('list', JSON.stringify(list));
+};
 
 const render = () => {
   list.forEach((task) => {
@@ -21,6 +26,19 @@ const render = () => {
     li.append(checkbox);
     li.append(label);
     taskList.appendChild(li);
+
+    checkbox.addEventListener('change', (e) => {
+      const taskAtHand = e.target.parentNode;
+      if (e.target.checked === true) {
+        updateToTrue(task);
+        taskAtHand.classList.add('crossed');
+        record();
+      } else {
+        updateToFalse(task);
+        taskAtHand.classList.remove('crossed');
+        record();
+      }
+    });
   });
 };
 
